2b9ad1b987906f118ed1bf19590d419a36b36518
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2024-07-30 11:59:29 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+number1 = 1234 // 10 % 100
+number2 = 5678 // 10 % 100
+print(number1 + number2)
+
+# 4th program
+number1 = 13.42
+number2 = 42.13
+print(int(number1 * 100 % 100) == int(number2) or int(number2 * 100 % 100) == int(number1))
